80f79c4cc8a57d5a3e59055439dd120cea2a741c
Chris Prather <chris@prather.org>
2012-01-24 16:07:32 -0500

    move to rsync into the perl directory

--- satori-build.sh
+++ satori-build.sh
@@ -1,8 +1,11 @@
 #!/bin/sh -x
 curl -L http://cpanmin.us | perl - --curl --wget -L perl5 --save-dists dists Task::Kensho
-find dists/ -type f | xargs -I % tar -xvzf %
+mkdir cpan
+cd cpan
+find ../dists/ -type f | xargs -I % tar -xvzf %
 for dir in `ls`; do  mv $dir `perl -E'$_ = shift; s/-\d.*$//; say;' $dir`; done
 find ./ -name Makefile.PL | xargs -I % mv % %.cpan
 rm -rf perl5/ dists/
-find . -type d -depth 1 | xargs -I % mv % ~/dev/perl/cpan/
+cd ..
+rsync -avz cpan/ ~/dev/perl/cpan/
 
